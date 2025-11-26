3f63238ba2e01bf2a1a58c315fbac13cc50b592a
LuckyShotAkaEdoardo <edoardolaurentino@gmail.com>
2025-11-26 13:04:03 +0100

    data

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { BiTableConfig } from './app/shared/bi-table/bi-table';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 })
 export class App {
   name = 'Angular';
+  tableConfig: BiTableConfig = {
+    headers: [
+      { label: 'Nome', key: 'nome' },
+      { label: 'Età', key: 'eta', type: 'number' },
+      { label: 'Registrato il', key: 'registrato', type: 'date' },
+      { label: 'Stato', key: 'stato', type: 'badge' }
+    ],
+    data: [
+      { nome: 'Mario', eta: 33, registrato: '2025-01-01', stato: 'ATTIVO' },
+      { nome: 'Lucia', eta: 27, registrato: '2024-11-28', stato: 'SOSPESO' }
+    ]
+  };
 }
 
 bootstrapApplication(App);
